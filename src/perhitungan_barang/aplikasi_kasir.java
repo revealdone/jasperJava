240f2239462335a4641ea68323621f20d2e99234
revealdone <muhamadwahidromdoni@gmail.com>
2024-07-30 12:12:58 +0700

    first commit

---
+++ src/perhitungan_barang/aplikasi_kasir.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perhitungan_barang;
+
+/**
+ *
+ * @author hp
+ */
+public class aplikasi_kasir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new form_login().show();
+        // TODO code application logic here
+    }
+    
+}
